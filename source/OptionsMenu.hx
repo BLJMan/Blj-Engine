0552248df6ea9df9ec7b967b1b1f66196f23b832
BLJMan <sonia.gabrielec@gmail.com>
2021-09-26 18:24:16 +0200

    ...

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -51,7 +51,7 @@ class OptionsMenu extends MusicBeatState
 
 		super.create();
 
-		openSubState(new OptionsSubState());
+		//openSubState(new OptionsSubState());
 	}
 
 	override function update(elapsed:Float)
