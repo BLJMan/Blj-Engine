1f2a09db5b35e6f559e7f4cfda940ba89aceb977
BLJMan <sonia.gabrielec@gmail.com>
2021-09-26 18:26:47 +0200

    very different trust me

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -1,36 +1,112 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.FlxG;
 import flixel.graphics.frames.FlxAtlasFrames;
+
+class CharacterSetting
+{
+	public var x(default, null):Int;
+	public var y(default, null):Int;
+	public var scale(default, null):Float;
+	public var flipped(default, null):Bool;
+
+	public function new(x:Int = 0, y:Int = 0, scale:Float = 1.0, flipped:Bool = false)
+	{
+		this.x = x;
+		this.y = y;
+		this.scale = scale;
+		this.flipped = flipped;
+	}
+}
 
 class MenuCharacter extends FlxSprite
 {
-	public var character:String;
+	private static var settings:Map<String, CharacterSetting> = [
+		'bf' => new CharacterSetting(0, -20, 1.0, true),
+		'gf' => new CharacterSetting(50, 80, 1.5, true),
+		'dad' => new CharacterSetting(-15, 130),
+		'spooky' => new CharacterSetting(20, 40, 1.15),
+		'pico' => new CharacterSetting(0, 10, 1.1, true),
+		'mom' => new CharacterSetting(-30, 140, 0.85),
+		'parents-christmas' => new CharacterSetting(100, 130, 1.8),
+		'senpai' => new CharacterSetting(-40, -45, 1.4),
+		'tankman' => new CharacterSetting(-70, -20, 1)
+	];
 
-	public function new(x:Float, character:String = 'bf')
+	private var flipped:Bool = false;
+	//questionable variable name lmfao
+	private var goesLeftNRight:Bool = false;
+	private var danceLeft:Bool = false;
+	private var character:String = '';
+
+	public function new(x:Int, y:Int, scale:Float, flipped:Bool)
 	{
-		super(x);
+		super(x, y);
+		this.flipped = flipped;
 
-		this.character = character;
+		antialiasing = true;
 
-		var tex = Paths.getSparrowAtlas('campaign_menu_UI_characters');
-		frames = tex;
+		frames = Paths.getSparrowAtlas('campaign_menu_UI_characters');
 
 		animation.addByPrefix('bf', "BF idle dance white", 24);
-		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24, false);
-		animation.addByPrefix('gf', "GF Dancing Beat WHITE", 24);
+		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24);
+		animation.addByPrefix('gf-left', 'GF Dancing Beat WHITE', 24);
+		animation.addByPrefix('gf-right', 'GF Dancing Beat WHITE',  24);
 		animation.addByPrefix('dad', "Dad idle dance BLACK LINE", 24);
-		animation.addByPrefix('spooky', "spooky dance idle BLACK LINES", 24);
+		animation.addByPrefix('spooky-left', 'spooky dance idle BLACK LINES', 24);
+		animation.addByPrefix('spooky-right', 'spooky dance idle BLACK LINES', 24);
 		animation.addByPrefix('pico', "Pico Idle Dance", 24);
 		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
-		animation.addByPrefix('parents-christmas', "Parent Christmas Idle Black Lines", 24);
+		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
 		animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24);
 		animation.addByPrefix("tankman", "Tankman Menu BLACK", 24);
-		// Parent Christmas Idle
 
-
-		animation.play(character);
-		antialiasing = true;
+		setGraphicSize(Std.int(width * scale));
 		updateHitbox();
+	}
+
+	public function setCharacter(character:String):Void
+	{
+		var sameCharacter:Bool = character == this.character;
+		this.character = character;
+		if (character == '')
+		{
+			visible = false;
+			return;
+		}
+		else
+		{
+			visible = true;
+		}
+
+		if (!sameCharacter) {
+			bopHead(true);
+		}
+
+		var setting:CharacterSetting = settings[character];
+		offset.set(setting.x, setting.y);
+		setGraphicSize(Std.int(width * setting.scale));
+		flipX = setting.flipped != flipped;
+	}
+
+	public function bopHead(LastFrame:Bool = false):Void
+	{
+		if (character == 'gf' || character == 'spooky') {
+			danceLeft = !danceLeft;
+
+			if (danceLeft)
+				animation.play(character + "-left", true);
+			else
+				animation.play(character + "-right", true);
+		} else {
+			//no spooky nor girlfriend so we do da normal animation
+			if (animation.name == "bfConfirm")
+				return;
+			animation.play(character, true);
+		}
+		if (LastFrame) {
+			animation.finish();
+		}
 	}
 }
