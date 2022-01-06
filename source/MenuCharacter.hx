b88aa10aeb1911d60238166dd1de8d8241b260fa
BLJMan <sonia.gabrielec@gmail.com>
2022-01-06 13:50:07 +0100

    fixed gf animations

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -51,11 +51,11 @@ class MenuCharacter extends FlxSprite
 
 		animation.addByPrefix('bf', "BF idle dance white", 24);
 		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24);
-		animation.addByPrefix('gf-left', 'GF Dancing Beat WHITE', 24);
-		animation.addByPrefix('gf-right', 'GF Dancing Beat WHITE',  24);
+		animation.addByIndices('gf-left', 'GF Dancing Beat WHITE', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+		animation.addByIndices('gf-right', 'GF Dancing Beat WHITE', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		animation.addByPrefix('dad', "Dad idle dance BLACK LINE", 24);
-		animation.addByPrefix('spooky-left', 'spooky dance idle BLACK LINES', 24);
-		animation.addByPrefix('spooky-right', 'spooky dance idle BLACK LINES', 24);
+		animation.addByIndices('spooky-left', 'spooky dance idle BLACK LINES', [0, 2, 6], "", 12, false);
+		animation.addByIndices('spooky-right', 'spooky dance idle BLACK LINES', [8, 10, 12, 14], "", 12, false);
 		animation.addByPrefix('pico', "Pico Idle Dance", 24);
 		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
 		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
