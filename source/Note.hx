e151d873c98af386ff5d4e97ae156f400444f514
BLJMan <sonia.gabrielec@gmail.com>
2022-01-23 20:45:42 +0100

    lua shit

--- source/Note.hx
+++ source/Note.hx
@@ -30,6 +30,8 @@ class Note extends FlxSprite
 
 	public var gfNote:Bool = false;
 
+	public var modifiedByLua:Bool = false;
+
 	public var noteScore:Float = 1;
 
 	public var colorSwap:ColorSwap;
@@ -42,7 +44,7 @@ class Note extends FlxSprite
 	//public static var RED_NOTE_TWO:Int = 4;
 
 
-	public function new(strumTime:Float, noteData:Int, ?prevNote:Note, ?sustainNote:Bool = false, noteType:Int = 0, ?editor:Bool = false)
+	public function new(strumTime:Float, noteData:Int, ?prevNote:Note, ?sustainNote:Bool = false, noteType:Int = 0, ?editor:Bool = true)
 	{
 		super();
 
@@ -241,13 +243,13 @@ class Note extends FlxSprite
 		if (mustPress)
 		{
 			// The * 0.5 is so that it's easier to hit them too late, instead of too early
-			if (strumTime > Conductor.songPosition - (Conductor.safeZoneOffset / 2)
+			if (strumTime > Conductor.songPosition - Conductor.safeZoneOffset
 				&& strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * 0.5))
 				canBeHit = true;
 			else
 				canBeHit = false;
 
-			if (strumTime < Conductor.songPosition - (Conductor.safeZoneOffset / 2)  && !wasGoodHit)
+			if (strumTime < Conductor.songPosition - Conductor.safeZoneOffset && !wasGoodHit)
 				tooLate = true;
 		}
 		else
