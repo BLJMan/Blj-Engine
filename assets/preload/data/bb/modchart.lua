02881f195fa2527894d68b016614ed8720235e4c
BLJMan <sonia.gabrielec@gmail.com>
2022-01-23 20:40:01 +0100

    lua

---
+++ assets/preload/data/bb/modchart.lua
@@ -0,0 +1,24 @@
+function start (song)
+    hideUpperHUD = true
+end
+
+function beatHit (beat)
+
+    if curBeat == 1 then 
+        for i = 0,3 do
+           tweenPosOut(i, -300, _G['defaultStrum'..i..'Y'], 2, (i / 15))
+        end
+        for i = 4,7 do 
+            tweenPosOut(i, ((screenWidth / 2) - 690) + (115 * i), _G['defaultStrum'..i..'Y'], 2, (i / 15))
+        end
+    end
+
+end
+
+function stepHit (step)
+    if curStep == 20 then
+        setActorX(850, "boyfriend")
+        setActorY(570, "boyfriend")
+    end
+
+end
---
+++ assets/preload/data/triple-trouble/modchart.lua
@@ -0,0 +1,32 @@
+function stepHit (step)
+    if curStep == 1040 then 
+        setActorX(20 - 200, "dad")
+        setActorY(-94.75 + 100, "dad")
+        setActorX(502.45 + 200, "boyfriend")
+        setActorY(370.45, "boyfriend")
+    end
+    if curStep == 1296 then 
+        setActorX(100, "dad")
+        setActorY(400, "dad")
+        setActorX(770, "boyfriend")
+        setActorY(450, "boyfriend")
+    end
+    if curStep == 2320 then 
+        setActorX(20 - 200, "dad")
+        setActorY(-94.75 + 100, "dad")
+        setActorX(502.45 + 200, "boyfriend")
+        setActorY(370.45, "boyfriend")
+    end
+    if curStep == 2823 then 
+        setActorX(20 - 200, "dad")
+        setActorY(30 + 200, "dad")
+        setActorX(466.1 + 200, "boyfriend")
+        setActorY(685.6 - 250, "boyfriend")
+    end
+    if curStep == 4111 then 
+        setActorX(20 - 200, "dad")
+        setActorY(-94.75 + 100, "dad")
+        setActorX(502.45 + 200, "boyfriend")
+        setActorY(370.45, "boyfriend")
+    end
+end
