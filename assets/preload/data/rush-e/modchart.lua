82a01fd701dabb78aa884249c7f212eac9fd0ab9
BLJMan <sonia.gabrielec@gmail.com>
2022-08-05 20:41:13 +0200

    updated assets

---
+++ assets/preload/data/rush-e/modchart.lua
@@ -0,0 +1,11 @@
+function start (song)
+    hideTimeAndRating = true
+end
+
+function beatHit (beat)
+	if curBeat == 1 then 
+        for i = 0,3 do
+           tweenPosOut(i, -300, _G['defaultStrum'..i..'Y'], 2, (i / 15))
+        end
+    end
+end
